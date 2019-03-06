f2922717161cecacb6234a54ea9df8e4cd8beeb8
Preyansh Mitharwal <jonysikar0007@gmail.com>
2019-03-06 22:15:03 +0530

    learning difference between interpolation and property binding in Angular using HTML select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface KeyValue {
+  key: string | number;
+  value: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-test-app';
+
+  carsWithStringKeys: KeyValue[] = [
+    {
+      key: 'tyt',
+      value: 'Toyota'
+    },
+    {
+      key: 'frd',
+      value: 'ford'
+    },
+    {
+      key: 'mhr',
+      value: 'Mahindra'
+    },
+    {
+      key: 'mrt',
+      value: 'Maruti'
+    },
+    {
+      key: 'vgn',
+      value: 'Volkeswagon'
+    },
+    {
+      key: 'nsn',
+      value: 'Nissan'
+    }
+  ];
+
+  carsWithIntegerKeys: KeyValue[] = [
+    {
+      key: 1,
+      value: 'Toyota'
+    },
+    {
+      key: 2,
+      value: 'ford'
+    },
+    {
+      key: 3,
+      value: 'Mahindra'
+    },
+    {
+      key: 4,
+      value: 'Maruti'
+    },
+    {
+      key: 5,
+      value: 'Volkeswagon'
+    },
+    {
+      key: 6,
+      value: 'Nissan'
+    }
+  ];
+
+  selectedCarWithStringKey = 'tyt';
+
+  selectedCarsWithStringKeys: string[] = ['tyt', 'mhr'];
+
+  selectedCarWithIntegerKey = 1;
+
+  selectedCarsWithIntegerKeys: number[] = [1, 4];
+
 }
